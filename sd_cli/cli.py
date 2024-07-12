04376ad46403507403212d012147e1eb84a3df3b
GAMch1k <gammail404@gmail.com>
2024-07-12 14:54:15 +0200

    Added colors and tested with passphrase

--- sd_cli/cli.py
+++ sd_cli/cli.py
@@ -1,28 +1,43 @@
 import os
-import subprocess
 
-def main():
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def main():    
     os.system('cls' if os.name == 'nt' else 'clear')
 
-    print("Welcome to the SKFX DEV cli")
+    print(f"Welcome to the {bcolors.OKGREEN}SKFX DEV{bcolors.ENDC} cli")
     print()
 
-    name = input("Enter project name: ")
-    git = input("GitHub repo origin: ")
-    print()
-    print("Choose project type")
-    print("1 - aiogram3 bot")
-    print("2 - backend")
-    pr_type = int(input("Insert number here: ")) - 1
-
+    name = input(f"Enter project name: {bcolors.OKCYAN}")
+    git = input(f"{bcolors.ENDC}GitHub repo origin: {bcolors.OKCYAN}")
+    print(f"{bcolors.ENDC}Choose project type")
+    print(f"{bcolors.OKBLUE}1{bcolors.ENDC} - aiogram3 bot")
+    print(f"{bcolors.OKBLUE}2{bcolors.ENDC} - backend {bcolors.FAIL}(not working yet!){bcolors.ENDC}")
+    pr_type = int(input(f"Insert number here:  {bcolors.OKCYAN}")) - 1
+    print(bcolors.ENDC)
+    
+    if pr_type == 1: 
+        quit()
+    
     templates = [
         {
             "origin": "git@github.com:skfxio/tgbot-template.git",
-            "name": "aiogram3 bot"
+            "name": "aiogram3 bot",
+            "dir": "tgbot-template",
         },
         {
             "origin": "git@github.com:skfxio/sdlib.git",
-            "name": "backend"
+            "name": "backend",
+            "dir": "sdlib",
         },
     ]
 
@@ -31,20 +46,20 @@ def main():
 
     print("Is everything correct?")
     print()
-    print("Name:", name)
-    print("Repo:", git)
-    print("Project type:", templates[pr_type]['name'])
+    print(f"Name:{bcolors.OKGREEN}", name, bcolors.ENDC)
+    print(f"Repo:{bcolors.OKGREEN}", git, bcolors.ENDC)
+    print(f"Project type:{bcolors.OKGREEN}", templates[pr_type]['name'], bcolors.ENDC)
     print()
-    correct = input("[y/n]  ")
+    correct = input(f"[{bcolors.OKGREEN}y{bcolors.ENDC}/{bcolors.FAIL}n{bcolors.ENDC}]  ")
     if correct.lower() != "y":
-        print("Quiting...")
+        print(f"{bcolors.FAIL}Quiting...{bcolors.ENDC}")
         quit()
 
     os.system('cls' if os.name == 'nt' else 'clear')
 
     os.system(f"git clone {templates[pr_type]['origin']}")
-    print("Renaming: tgbot-template ->", name)
-    os.rename("tgbot-template", name)
+    print(f"{bcolors.OKGREEN}Renaming: {templates[pr_type]['dir']} -> {name}{bcolors.ENDC}")
+    os.rename(templates[pr_type]['dir'], name)
 
     os.chdir(name)
 
@@ -55,7 +70,6 @@ def main():
     os.system(f"git branch -D master")
     os.system(f"git branch -m master")
     
-    # print(subprocess.getstatusoutput("git push --force origin master"))
     
     os.system(f"git push --force origin master")
 
@@ -71,15 +85,14 @@ def main():
     print()
     print()
 
-    print("Finished! You can start bot with commands:")
+    print(f"{bcolors.OKGREEN}Finished!{bcolors.ENDC} You can start bot with commands:")
     print(f"cd {name}")
 
     if os.name == "posix":
         print("source ./venv/bin/activate")
     if os.name == "nt":
-        os.system(".\\venv\\Scripts\\activate")
+        print(".\\venv\\Scripts\\activate")
         
-    print("python -m bot")
+    print(f"python -m bot")
 
-
-# main()
+main()
